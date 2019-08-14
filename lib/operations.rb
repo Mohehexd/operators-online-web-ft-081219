cf6f2f176c8897f70b4debd79781e0a41fc5221d
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-14 21:47:25 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -17,10 +17,7 @@ end
 
 def not_safe?(speed)
   
-(speed == 50) && (speed < 40) && (speed >= 60) ? (return true) : (return false)
+(speed == 50) || (speed < 40) && (speed >= 60) ? (return true) : (return false)
 
 end
 
-(speed < 40) ? (return true) : (return false)
-  
-(speed == 50) ? (return false) : (return true)
